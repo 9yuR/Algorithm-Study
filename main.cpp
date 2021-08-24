88eaecbc8dc90e778c0300005e74e79d40df7cb9
Git-GR <70843764+Git-GR@users.noreply.github.com>
2021-08-24 19:22:42 +0900

    BOJ 17087
    문제
    수빈이는 동생 N명과 숨바꼭질을 하고 있다. 수빈이는 현재 점 S에 있고, 동생은 A1, A2, ..., AN에 있다.
    
    수빈이는 걸어서 이동을 할 수 있다. 수빈이의 위치가 X일때 걷는다면 1초 후에 X+D나 X-D로 이동할 수 있다. 수빈이의 위치가 동생이 있는 위치와 같으면, 동생을 찾았다고 한다.
    
    모든 동생을 찾기위해 D의 값을 정하려고 한다. 가능한 D의 최댓값을 구해보자.
    
    입력
    첫째 줄에 N(1 ≤ N ≤ 105)과 S(1 ≤ S ≤ 109)가 주어진다. 둘째 줄에 동생의 위치 Ai(1 ≤ Ai ≤ 109)가 주어진다. 동생의 위치는 모두 다르며, 수빈이의 위치와 같지 않다.
    
    출력
    가능한 D값의 최댓값을 출력한다.

--- main.cpp
+++ main.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-bool Pcheck(int num){
-    int i = 3;
-    if(num == 3)
-        return true;
-    while(i < num){
-        if(num % i == 0)
-            return false;
-        i += 2;
+int gcd(int a, int b) {
+    while (b > 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
     }
-    return true;
-};
+    return a;
+}
 
 int main(void) {
-    int n = 0;
-    int a, b;
-    while( n != 0 ){
-        cin >> n;
-        a = 0;
-        for(int i = 3; i <= n; i++){
-            if(Pcheck(i) && Pcheck(n - i)){
-                a = i;
-                b = n - i;
-                cout << n << " = " << a << " + " << b << endl;
-                break;
-            }
-        }
-        if(a == 0)
-            cout << "Goldbach's conjecture is wrong." << endl;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N, S, A;
+    int GCD;
+    vector<int> vec;
+    cin >> N >> S;
+    for (int i = 0; i < N; i++) {
+        cin >> A;
+        vec.push_back(abs(A-S));
     }
+    GCD = gcd(vec[0], vec[1]);
+    for (int i = 2; i < vec.size(); i++) {
+        if (GCD == 1)
+            break;
+        GCD = gcd(GCD, vec[i]);
+    }
+    cout << GCD << endl;
     return 0;
 }
